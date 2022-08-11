7fbdc0d9e31544d524d79c027dbda85b9d50c40f
pctinkychausky <pctinkychausky@gmail.com>
2022-08-11 13:42:55 +0100

    rightbar finished

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -12,7 +12,24 @@ function Rightbar() {
       </div>
 
       <div className="rightbar__Container">
-        <h2>What's happening</h2>
+        <h2 className="about">About Me</h2>
+        <h3>Hello! My name is Jason Chau and I am a Junior Web Developer.</h3>
+        <h5>
+          I am motivated in solving real-world challenges by analytical thinking
+          and my social studies mindset .
+        </h5>
+        <h5>
+          After 8 years working as a consultant roles of financial and testing
+          industry, I decided to take a career change to pursue a goal of
+          becoming a software engineer.
+        </h5>
+        <h2 className="contact">Contact</h2>
+        <h4>Email</h4>
+        <h5>pctinkychausky@gmail.com</h5>
+        <h4>GitHub</h4>
+        <h5>htth5s://github.com/h5ctinkychausky/</h5>
+        <h4>Moblie</h4>
+        <h5>+44(0)7878925194</h5>
       </div>
     </div>
   );
