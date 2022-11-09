358daa2674ab87aa7ba96711ceebcf31ca87797a
pctinkychausky <pctinkychausky@gmail.com>
2022-11-09 17:55:28 +0000

    non-float verison

--- src/components/Feeds/Post/Post.jsx
+++ src/components/Feeds/Post/Post.jsx
@@ -79,83 +79,57 @@ function Post({ url, displayName, username, src, text, color, avatar, title }) {
             <p>{text}</p>
           </div>
         </div>
-        {/* <motion.div
-          className="cardWrapper"
-          variants={cardVariants}
-          initial={{ y: -50 }}
-          animate={{ y: -10 }}
-          transition={{ delay: 0.1, type: "spring", stiffness: 120 }}
-          whileHover="hover"
-          whileSpring="spring"
-          style={{
-            x,
-            y,
-            rotateX,
-            rotateY,
-            z: 200,
-          }}
-        > */}
+
         <animated.div
           className="cardsContainer"
           ref={ref}
-          onMouseEnter={() => setHovered(true)}
-          onMouseMove={({ clientX, clientY }) => {
-            // Get mouse x position within card
-            const x =
-              clientX -
-              (ref.current.offsetLeft -
-                (window.scrollX ||
-                  window.pageXOffset ||
-                  document.body.scrollLeft));
+          // onMouseEnter={() => setHovered(true)}
+          // onMouseMove={({ clientX, clientY }) => {
+          //   // Get mouse x position within card
+          //   const x =
+          //     clientX -
+          //     (ref.current.offsetLeft -
+          //       (window.scrollX ||
+          //         window.pageXOffset ||
+          //         document.body.scrollLeft));
 
-            // Get mouse y position within card
-            const y =
-              clientY -
-              (ref.current.offsetTop -
-                (window.scrollY ||
-                  window.pageYOffset ||
-                  document.body.scrollTop));
+          //   // Get mouse y position within card
+          //   const y =
+          //     clientY -
+          //     (ref.current.offsetTop -
+          //       (window.scrollY ||
+          //         window.pageYOffset ||
+          //         document.body.scrollTop));
 
-            // Set animated values based on mouse position and card dimensions
-            const dampen = 100; // Lower the number the less rotation
-            const xys = [
-              -(y - ref.current.clientHeight / 2) / dampen, // rotateX
-              (x - ref.current.clientWidth / 2) / dampen, // rotateY
-              1.07, // Scale
-            ];
+          //   console.log("x,y", x, y);
+          //   console.log("y");
 
-            // Update values to animate to
-            setAnimatedProps({ xys: xys });
-          }}
-          onMouseLeave={() => {
-            setHovered(false);
-            // Set xys back to original
-            setAnimatedProps({ xys: [0, 0, 1] });
-          }}
+          //   // Set animated values based on mouse position and card dimensions
+          //   const dampen = 100; // Lower the number the less rotation
+          //   const xys = [
+          //     -(y - ref.current.clientHeight / 2) / dampen, // rotateX
+          //     (x - ref.current.clientWidth / 2) / dampen, // rotateY
+          //     1.07, // Scale
+          //   ];
+
+          //   // Update values to animate to
+          //   setAnimatedProps({ xys: xys });
+          // }}
+          // onMouseLeave={() => {
+          //   setHovered(false);
+          //   // Set xys back to original
+          //   setAnimatedProps({ xys: [0, 0, 1] });
+          // }}
           style={{
             // If hovered we want it to overlap other cards when it scales up
-            zIndex: isHovered ? 2 : 1,
-            // Interpolate function to handle css changes
-            transform: animatedProps.xys.interpolate(
-              (x, y, s) =>
-                `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
-            ),
+            // zIndex: isHovered ? 2 : 1,
+            // // Interpolate function to handle css changes
+            // transform: animatedProps.xys.interpolate(
+            //   (x, y, s) =>
+            //     `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+            // ),
             background: color,
           }}
-          // onMouseMove={($e) => {
-          //   $e.target.style.transform = `rotateY(${xAxis * 5}deg) rotateX(${
-          //     yAxis * 5
-          //   }deg)`;
-          //   mouseover($e);
-          // }}
-          // onMouseLeave={($e) => {
-          //   $e.target.style.transform = `rotateY(0deg) rotateX(0deg)
-          //   `;
-          //   mouseleave($e);
-          // }}
-          // style={{
-          //   background: color,
-          // }}
         >
           <div className="inner__cardsContainer">
             <Projects
@@ -172,8 +146,6 @@ function Post({ url, displayName, username, src, text, color, avatar, title }) {
             />
           </div>
         </animated.div>
-        {/* </motion.div> */}
-
         <div className="post__footer">
           <ChatBubbleOutlineIcon fontSize="small" />
           <RepeatIcon fontSize="small" />
