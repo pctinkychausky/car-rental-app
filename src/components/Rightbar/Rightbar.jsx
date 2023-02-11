42db8ec31241bb63dbb5565814294513960ea51d
pctinkychausky <pctinkychausky@gmail.com>
2023-02-11 07:11:08 +0000

    stacks and CV update

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -13,16 +13,32 @@ function Rightbar() {
 
       <div className="rightbar__Container">
         <h2 className="about">About Me</h2>
-        <h3>Hello! My name is Jason Chau and I am a Junior Web Developer.</h3>
-        <h5>
-          I am motivated in solving real-world challenges by analytical thinking
-          and my social studies mindset .
-        </h5>
-        <h5>
-          After 8 years working as a consultant roles of financial and testing
-          industry, I decided to take a career change to pursue a goal of
-          becoming a software engineer.
-        </h5>
+        <h3>
+          Hello! My name is Jason Chau and I am looking for a Junior Web
+          Developer Job.
+        </h3>
+        <div>
+          <h2 className="Stacks">Stacks</h2>
+          <h5>
+            HTML5 | CSS3 | Responsive design | Flexbox/CSS Grid | SASS | CSS
+            frameworks Bootstrap | TailwindCSS | OAuth | SSO
+          </h5>
+          <h5>JavaScript: ES6+ | AJAX & API | REST | GraphQL | OOP</h5>
+          <h5>
+            React | Next.js | Vue2 |Nuxt,js| TailwindCSS | hooks, context,
+            Material UI
+          </h5>
+          <h5>Node.js: Vanilla & Express.js</h5>
+          <h5>
+            Databases SQL - Postgres | NoSQL - MongoDB with Mongoose.js, Redis,
+            | Firebase |
+          </h5>
+          <h5>
+            DevOps | Deployment | Docker & Kubernetes | Heroku (inc. pipelines)|
+            serverless | Netlify
+          </h5>
+          <h5>Testing: TypeScript, Cypress, Vitest, Vite</h5>
+        </div>
         <h2 className="contact">Contact</h2>
         <h4>Email</h4>
         <h5>pctinkychausky@gmail.com</h5>
@@ -32,7 +48,7 @@ function Rightbar() {
         <h5>https://github.com/pctinkychausky/</h5>
         <h4>
           CV ({" "}
-          <a href="https://drive.google.com/file/d/1Tyrz76QUCLx6nCpTj5o380_mIt70apKj/view?usp=sharing">
+          <a href="https://drive.google.com/file/d/17qNfZrBZYT4JcQ6TWOE9Ca12OyuipSZR/view?usp=sharing">
             PDF verison
           </a>
           )
